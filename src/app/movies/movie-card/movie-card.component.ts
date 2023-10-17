96361b4c966d995c9b5737b7eedaef63740b3335
sarahmhd <sarahmhd213199@gmail.com>
2023-10-17 13:22:22 +0200

    movie card component

--- src/app/movies/movie-card/movie-card.component.ts
+++ src/app/movies/movie-card/movie-card.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { faEllipsis, faHeart as golden } from '@fortawesome/free-solid-svg-icons'
+
+import { MovieInterface } from 'src/app/interfaces/movie-interface';
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
 
 @Component({
   selector: 'app-movie-card',
@@ -6,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./movie-card.component.css']
 })
 export class MovieCardComponent {
+  @Input() movie!: MovieInterface
+  faEllipsis = faEllipsis
+  goldenHeart = golden
+  faHeart = faHeart
 
+
+  checkAdded() {
+
+  }
 }
--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -1,4 +1,8 @@
+import { faGreaterThan, faLessThan } from '@fortawesome/free-solid-svg-icons'
+
 import { Component } from '@angular/core';
+import { HttpService } from 'src/app/shared/services/http.service';
+import { MovieInterface } from 'src/app/interfaces/movie-interface';
 
 @Component({
   selector: 'app-movies-list',
@@ -6,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./movies-list.component.css']
 })
 export class MoviesListComponent {
+  faArrowLeft = faLessThan
+  faArrowRight = faGreaterThan
+
+
+  fakeArr(movies: number) {
+    return Array(movies).fill(0)
+  }
+
 
 }
