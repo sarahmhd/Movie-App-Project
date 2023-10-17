403df63975b1b60d688ae38f92906535bd0a4cdb
omarcamir <omarcamirr@gmail.com>
2023-10-17 05:58:48 +0300

    add movie-details

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { MovieDetailsCardComponent } from './movie-details-card/movie-details-card.component';
-import { MoviesDetailsComponent } from './movies-details/movies-details.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { MoviesListComponent } from './movies-list/movies-list.component';
 import { RecommendatoinComponent } from './recommendatoin/recommendatoin.component';
 import { WatchListCardComponent } from './watch-list-card/watch-list-card.component';
 import { WatchListComponent } from './watch-list/watch-list.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -15,15 +16,24 @@ import { WatchListComponent } from './watch-list/watch-list.component';
   declarations: [
     MovieCardComponent,
     MovieDetailsCardComponent,
-    MoviesDetailsComponent,
     MovieDetailsComponent,
     MoviesListComponent,
     RecommendatoinComponent,
     WatchListCardComponent,
-    WatchListComponent
+    WatchListComponent,
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FontAwesomeModule,
+    RouterModule,
+  ],
+    exports: [
+      MovieCardComponent,
+      MovieDetailsComponent,
+      MoviesListComponent,
+      RecommendatoinComponent,
+      WatchListCardComponent,
+      WatchListComponent
+    ]
 })
 export class MoviesModule { }
